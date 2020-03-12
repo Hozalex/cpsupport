593c73b5df9242558f947295d8f83dc858f57e3d
Aleksandr Khozyainov <hozalex@gmail.com>
2020-03-12 19:00:03 +0300

    autocommit

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from app.model.Company import Company
+from docx import Document
+import re
+import json
+
+
+def file_extract():
+    doc = Document('C:/Users/kav1302/PycharmProjects/cpsupport/test.DOCX')
+    email = None
+    for p in doc.paragraphs:
+        text = p.text
+        print(text)
+        email = re.search(r'[a-z0-9.\-+_]+@[a-z0-9.\-+_]+\.[a-z]+', text)
 
 
 def main():
-    company = Company('XCompanyX')
-    print(company.name)
+    file_extract()
 
 
 main()
