9f0dba6c7c8c95f7cd3718b1ef1aedaae4b1a40c
Hozalex <hozalex@gmail.com>
2021-01-24 20:35:33 +0300

    Update rest.py

--- app/model/rest.py
+++ app/model/rest.py
@@ -8,19 +8,19 @@ class Rest:
     issues = '/api/v1/issues/120/attachments/656945'
     company = '/api/v1/companies/'
     id = '44'
-    token = {'api_token': 'f51f7d713e28f37aecb37bfb99e600ce1448d2df'}
+    token = {'api_token': ''}
     company_json = None
 
     def post(self):
         response_post = requests.patch(
-            'https://sd.cpsupport.ru/api/v1/companies/44?api_token=f51f7d713e28f37aecb37bfb99e600ce1448d2df',
+            'https://sd.cpsupport.ru/api/v1/companies/44?api_token=',
             data=json.dumps(self.company_json), headers={'Content-Type': 'application/json'})
 
         print('response post', response_post)
 
     def get(self):
         response = requests.get(self.url + self.company,
-                                params={'api_token': 'f51f7d713e28f37aecb37bfb99e600ce1448d2df', 'name': 'Алтекс Софт'})
+                                params={'api_token': '', 'name': 'Алтекс Софт'})
         self.company_json = response.json()
         self.company_json['additional_name'] = 'Altx-Soft'
         print(response)
